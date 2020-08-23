bc92cfb95c8e417e1c4b8ef32197d479d527ad2d
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-08-23 18:40:15 -0300

    Styling, no vol sliders

--- js/sketch3.js
+++ js/sketch3.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1280, 720);
+  createCanvas(windowWidth, windowHeight);
   //canvas.position (70,40);
   vid = createVideo('gsg.mp4');
   vid2 = createVideo('gsg.mp4');
@@ -9,12 +9,12 @@ function setup() {
   vid2.loop();
   //canvas.style('z-index','+1');
   //canvas.parent('p5');
-  slider = createSlider(0, 1, 0.5, 0.1);
+  // slider = createSlider(0, 1, 0.5, 0.1);
 }
 
 function draw() {
-  background(250);
-  vid.volume(slider.value());
+  // background(250);
+  // vid.volume(slider.value());
   vid2.volume(0);
   image(vid2, 10, 10);
   filter(INVERT);
--- js/sketch5.js
+++ js/sketch5.js
@@ -10,12 +10,12 @@ function setup() {
 
   //canvas.style('z-index','+1');
   //canvas.parent('p5');
-  slider = createSlider(0, 1, 0.5, 0.1);
+  // slider = createSlider(0, 1, 0.5, 0.1);
 }
 
 function draw() {
-  background(250);
-  vid.volume(slider.value());
+  // background(250);
+  // vid.volume(slider.value());
   vid2.volume(0);
   image(vid2, 10, 10);
   filter(INVERT);
